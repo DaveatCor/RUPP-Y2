fa3180695fff738c5ed03d39b52b86ca217c6751
DaveatCor <condaveat@gmail.com>
2020-12-31 09:16:14 +0700

    learn input and output

---
+++ week4/december/week4/test2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	int id; float average; char sex,name[20];
+	cout<<"Input id: "; cin>>id;
+	cout<<"My id: "<<id;
+	getch();
+}
