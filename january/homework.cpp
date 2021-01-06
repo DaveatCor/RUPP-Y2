005a9185d090aaabb3c256f950b1e656e0acd9ec
DaveatCor <condaveat@gmail.com>
2021-01-06 10:02:48 +0700

    add home work

---
+++ january/homework.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	int n, factorial;
+	cout<<"Factorial";
+	cin>>n;
+	
+	cout<<n;
+	for (int i = 1; i <= n; i++){
+		for ( int j = 1; j <= i; j++ ){
+			factorial *= j;
+		}
+		fa
+	}
+	getch();
+}
