19758d43f633f854bee42df286feccf6bf067d9e
DaveatCor <condaveat@gmail.com>
2021-01-26 09:01:57 +0700

    header file, chapter2 class, date 26/Jan/2021

---
+++ chapter1/january/week2_test/14_array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void input(float a[], int n){ 
+ 	for (int i=0; i<n; i++){
+		cout<<"..........\n";
+		cin>>a[i];
+	}
+ }
+
+void output(float a[], int n){
+	for (int i = 0; i< n; i++){
+		cout<<a[i]<<"\t";
+	}
+}
+
+int main(){
+	float x[100]; // without initialze size => x[] = {34, 78, 92, 16, 29, 55, 15};
+	// Scientific	
+	int n;
+	cout<<"Number of elements: "; cin>>n;
+	input(x, n);
+	output(x, n);
+	getch();
+}
