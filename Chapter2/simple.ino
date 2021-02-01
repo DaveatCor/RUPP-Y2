72c3fb548f5a73ef7d90d7187abcde92bb6fa118
DaveatCor <condaveat@gmail.com>
2021-02-01 12:56:45 +0700

    test UnAduSum

---
+++ Chapter2/simple.ino
@@ -0,0 +1,16 @@
+
+/*	This is a default program--
+	Use File->Load Prog to load a different program
+*/
+
+int count;
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println("Hello world");
+}
+
+void loop()
+{
+}
