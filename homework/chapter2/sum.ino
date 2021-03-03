3aa3ebf44b75631a572fc653298e0af008d89247
DaveatCor <condaveat@gmail.com>
2021-03-03 08:09:20 +0700

    finish homework

---
+++ homework/chapter2/sum.ino
@@ -0,0 +1,28 @@
+/*  This is a default program--
+  Use File->Load Prog to load a different program
+*/
+
+int array[5];
+int sum = 0;
+int i = 0;
+
+void setup()
+{
+	Serial.begin(9600); //open serial port 9600
+}
+
+void loop()
+{
+	if (Serial.available()){
+		if(i < 5){
+			array[i] = Serial.parseInt();
+			sum += array[i];
+			Serial.println(i);
+			i++;
+			
+			if (i == 5) {
+				Serial.print("Sum= "); Serial.println(sum);
+			}
+		}
+	}
+}
