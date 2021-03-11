bca7174d080e71e07f5cfe1791de088f96a69c3a
DaveatCor <condaveat@gmail.com>
2021-03-11 07:43:23 +0700

    add chapter3 LCD4 and example, date 11/Mar/21

---
+++ exercise/chapter3/LED4.ino
@@ -0,0 +1,16 @@
+
+/*	This is a default program--
+	Use File->Load Prog to load a different program
+*/
+
+int count;
+
+void setup()
+{
+	pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(2, 1);
+}
