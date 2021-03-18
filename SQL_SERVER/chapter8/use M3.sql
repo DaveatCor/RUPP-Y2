8e89a04cad75eeecbfeef6aa1e8763af2a34894b
DaveatCor <condaveat@gmail.com>
2021-03-18 11:42:41 +0700

    8.3.4 ALTER TABLE, created database ADDL( CREATE, DROP ), added and learn on ALTER (Edit on type), date 18/Mar/21

---
+++ SQL_SERVER/chapter8/use M3.sql
@@ -0,0 +1,26 @@
+USE M3
+
+-- CREATE TABLE Toys123(
+--     ToyID INTEGER NOT NULL,
+--     ToyName CHAR(30) NOT NULL,
+--     Price MONEY NOT NULL,
+--     Description CHAR(40) NOT NULL
+-- )
+
+-- -------------*"INSERT INTO ADDL"*-------------
+-- VALUES (1, 'Daveat', 'condaveat@gmail.com')
+
+-- -------------*"Query Data From TABLE"*----------
+-- SELECT * FROM ADDL
+
+-- -------------*"Add Column"*-------------
+-- ALTER TABLE ADDL
+-- ADD Email VARCHAR(30)
+
+-- -------------*"Add PRIMARY KEY"*-------------
+-- ALTER TABLE Toys123
+-- ADD CONSTRAINT toy123_pk PRIMARY KEY(ToyID)
+
+-- "Query"
+-- SELECT * FROM Toys123
+
