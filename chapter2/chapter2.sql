a3b4c29ee272fa943387a49e3afa801fe1d4224e
DaveatCor <condaveat@gmail.com>
2021-03-25 12:45:08 +0700

    re-learn chapter 2 (constraint), date 25/Mar/21

---
+++ chapter2/chapter2.sql
@@ -0,0 +1,15 @@
+-- CREATE DATABASE Chapter2
+Use Chapter2
+
+-- CREATE TABLE Toys(
+--     ToyID INTEGER,
+--     ToyName CHAR(30),
+--     Price MONEY, 
+--     Description CHAR(40)
+-- )
+
+-- ALTER TABLE Toys
+-- ADD PRIMARY KEY (ToyID)
+
+SELECT * 
+FROM Toys
