a63ded2c336b8f1458c79826217ac6f150cf4d75
DaveatCor <condaveat@gmail.com>
2021-03-26 11:18:40 +0700

    re-learn inheritance in_person.cpp(25/Mar/21), date 26/Mar/21

--- chapter3/example/in_person.cpp
+++ chapter3/example/in_person.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#Include <conio.h>
+#include <conio.h>
+#include <string.h>
 using namespace std;
 
 
@@ -7,21 +8,62 @@ class Person{
 	
 	private:
 		int id;
-		char *name;
+		char name[20];
 		char sex;
-		float average;
+		
 	public:
+		Person(){
+			id = 123;
+			strcpy(name, "Daveat");
+			sex = 'M';
+		}
+		
+		Person(int i, char* na, char s){
+			id = i;
+			strcpy(name, na);
+			sex = s;
+		}
+		
 		void input(){
-			
+			cout<<"Id: "; cin>> id;
+			cout<<"Name: "; 
+			// Clear buffer for String
+			cin.seekg(0, ios::end); cin.clear();
+			// Input string
+			cin.get(name, 20);
+			cout<<"Sex: "; cin>>sex;
 		}
 		
 		void output(){
-			
+			cout<<id<<"\t"<<name<<"\t"<<sex;
 		}
 };
 
 class Student:private Person{
 	
+	private:
+		float average;
+		
+	public:
+		Student(){
+			Person();
+			average = 20;
+		}
+		
+		Student(int i, char* na, char s, float avg){
+			Person(i, na, s);
+			average = avg;
+		}
+		
+		void input(){
+			Person::input();
+			cout<<"Average"; cin>>average;
+		}
+		
+		void output(){
+			Person::output();
+			cout<<"\t"<<average<<endl;
+		}
 };
 
 
@@ -49,30 +91,31 @@ void sort (Student a[], int n){
 		}
 } 
 
-void main(){
+main(){
 //	Student st(5555, "Tep Bopha", 'F', 85);
+	Student st;
 //	cout<<"Data of student: "<<endl;
 //	st.output();
-	/*	
+	
 	cout<<"Input data of Student: "<<endl;
 	st.input();
 	cout<<"Data of student: "<<endl;
-	st.output():
-	*/
+	st.output();
 	
-	int i;
 	
-	Student [5]={
-		Student(1, "Tep Bopha", 'F', 85),
-		Student(2, "Dara", 'M', 57),
-		Student(3, "Veasna", 'M', 47),
-		Student(4, "Mary", 'F', 68),
-		Student(5, "Channy", 'F', 99)
-	};
-	for (i = 0; i< 5; i++){
-		a[i].output();
-	}
-	sort(a, 5);
+//	int i;
+//	
+//	Student [5]={
+//		Student(1, "Tep Bopha", 'F', 85),
+//		Student(2, "Dara", 'M', 57),
+//		Student(3, "Veasna", 'M', 47),
+//		Student(4, "Mary", 'F', 68),
+//		Student(5, "Channy", 'F', 99)
+//	};
+//	for (i = 0; i< 5; i++){
+//		a[i].output();
+//	}
+//	sort(a, 5);
 	getch();
 }
 
