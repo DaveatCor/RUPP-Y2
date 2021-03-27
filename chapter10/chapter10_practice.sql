7066bdb1e738423b853ef56595e5ea0fa4a1fccf
DaveatCor <condaveat@gmail.com>
2021-03-27 08:55:23 +0700

    add chapter10 learn on website w3school(SQL function), date 27/Mar/21

---
+++ chapter10/chapter10_practice.sql
@@ -0,0 +1,11 @@
+-- Convert ASCII Code To Character
+-- SELECT CHAR(65) AS CodeToCharacter;
+
+-- Search Character and Return position index
+-- SELECT CHARINDEX('mer', 'Customer') AS MatchPosition;
+
+-- SELECT CONCAT('W3schools', '.com')
+-- SELECT 'W3schools' + '.com'
+
+
+
