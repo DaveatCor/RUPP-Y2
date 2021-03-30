6552e68a45674d50f15e2fac1171500f67fd1dd8
DaveatCor <condaveat@gmail.com>
2021-03-30 10:06:09 +0700

    add chapter 5 and project, date 30/Mar/21

---
+++ chapter5/example/keypad.ino
@@ -0,0 +1,131 @@
+#include<Keypad.h>
+#include<LiquidCrystal.h>
+
+// LCD
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int num1;
+int num2;
+int count1 = -1;
+int count2 = -1;
+
+char choose;
+
+int sum = 0;
+
+// Keypad
+const int ROWS = 4;
+const int COLS = 4;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3', '+'},
+  {'4', '5', '6', '-'},
+  {'7', '8', '9', '*'},
+  {'C', '0', '=', '/'},
+};
+
+byte rowPins[ROWS] = {10, 9, 8, 7};
+byte colPins[COLS] = {A0, A1, A2, A3};
+
+char customKey;
+
+Keypad customKeypad = Keypad(
+  makeKeymap(hexaKeys), 
+  rowPins,
+  colPins,
+  ROWS,
+  COLS
+);
+
+void setup() {
+  Serial.begin(9600);
+  
+  //lcd
+  analogWrite(6, 30);
+  lcd.begin(16, 2);
+  
+  lcd.setCursor(0, 0);
+  lcd.print("Group 4");
+  lcd.setCursor(0, 1);
+  lcd.print("Calculator");
+}
+void reset(){
+    count1 = -1;
+    count2 = -1;
+    num1 = 0;
+    num2 = 0;
+    sum = 0;
+}
+
+void loop() {
+  customKey = customKeypad.getKey();
+  
+  if (customKey != NO_KEY){
+
+    Serial.println(customKey, DEC);
+
+    if (count1 == -1){
+      lcd.clear();
+      count1 = 0;
+    }
+
+    if (customKey == '+'){
+      choose = '+';
+      lcd.clear();
+      count2 = 0;
+    } else if (customKey == '-'){
+      choose = '-';
+      lcd.clear();
+      count2 = 0;
+    } else if (customKey == '*'){
+      choose = '*';
+      lcd.clear();
+      count2 = 0;
+    } else if (customKey == '/'){
+      choose = '/';
+      lcd.clear();
+      count2 = 0;
+    } else if ( customKey == '='){
+      lcd.clear();
+
+      Serial.println(num1);
+      Serial.println(num2);
+      switch(choose){
+        case '+': sum = num1 + num2; break;
+        case '-': sum = num1 - num2; break;
+        case '*': sum = num1 * num2; break;
+        case '/': sum = num1 / num2; break;
+      }
+      
+      lcd.setCursor(0, 0);
+      lcd.print("Sum =");
+      
+      lcd.setCursor(6, 0);
+      lcd.print(0x00+sum);
+
+      reset();
+      
+    } else if (customKey == 'C'){
+      reset();
+
+      lcd.setCursor(0, 0);
+      lcd.print("Group 4");
+      lcd.setCursor(0, 1);
+      lcd.print("Calculator");
+      
+    } else if (count2 != -1){
+      lcd.setCursor(count2, 0);
+      lcd.print(customKey);
+
+      num2 += customKey;
+      count2++;
+    } else if (count1 != -1){
+      
+      lcd.setCursor(count1, 0);
+      lcd.print(customKey);
+    
+      num1 += customKey;
+      count1++;
+    }    
+  }
+}
