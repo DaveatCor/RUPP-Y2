576fd7aae74b4c5431cd3bc3fa83a457762938af
DaveatCor <condaveat@gmail.com>
2021-04-17 09:07:05 +0700

    re-learning chapter5, add table Computers and data, date 17/Apr/21

---
+++ chapter5/READEME.md
@@ -0,0 +1,3 @@
+5.1 WHERE Clause
+    - use for specify condition
+5.1.2 LOGICAL OPERATORS
---
+++ chapter5/chapter5.sql
@@ -0,0 +1,12 @@
+USE YEAR2
+
+-- CREATE TABLE Computers(
+--     SerialName CHAR(30) PRIMARY KEY NOT NULL,
+--     Brand CHAR(20) NOT NULL,
+--     Department CHAR(20) NOT NULL,
+--     OfficeNumber INTEGER NOT NULL 
+-- )
+
+SELECT *
+FROM Computers
+
