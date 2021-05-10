42faa1d6887086d6ba9879c830a23a0c16b0dee8
DaveatCor <condaveat@gmail.com>
2021-05-10 08:44:45 +0700

    finish homework, date 10/May/21

--- Semi2/homework/hw.cpp
+++ Semi2/homework/hw.cpp
@@ -14,11 +14,15 @@ class Electricity {
              }
              
              void output(){
-                  cout<<"Electricity total= "<<electricConsumption;
+                  cout<<"\nElectricity total= "<<electricConsumption;
              }
              
              float getElectric(){
                   return electricConsumption;
+             }
+             
+             void setElectric(float ele){
+                   electricConsumption = ele;
              }
 };
 
@@ -35,21 +39,35 @@ class Water {
              
              void output(){
                   
-                  cout<<"Water total= "<<waterConsumption;
+                  cout<<"\nWater total= "<<waterConsumption;
              }
              
              float getWater(){
                   return waterConsumption;
              }
              
+             void setWater(float water){
+                   waterConsumption = water;
+             }
+             
+      
+             
 };
 
-class Customer: private Electricity, private Water {
+class Customer: public Electricity, public Water {
+             
+      private:
+              int id;
+              char name[20];
       
       public:
              
-             Customer(int id, char *name, float ){
-                        id = 1;
+             Customer(int i, char *n, float elec, float water){
+                        id = i;
+                        strcpy(name, n);
+                        Electricity::setElectric(elec);
+                        Water::setWater(water);
+                        
              }
              
              void input(){
@@ -76,26 +94,85 @@ class Customer: private Electricity, private Water {
                   Water::output();
              }
              
-      private:
-              int id;
-              char name[20];
+             int getId() {
+                 return id;    
+             }
 };
 
 void findMostUseElectric(Customer cus[], int size){
-     int temp;
-     //for (int i = 0; i < size; i++){
-//         cus[i].electricity
-//     }
-}
-
-void findMostUseWater(){
-
-}
-
-void findMostUseElectricNWater(){
-
-}
-
- main(){
+     int i = 1;
+     Customer tmp = cus[0];
+     for ( ; i < size; i++){
+         if (cus[i].getElectric() > tmp.getElectric()){
+            tmp = cus[i];
+         } 
+     }
      
+     cout<<"\nUser that use a lot of electric\n";
+     tmp.output();
+}
+
+void findMostUseWater(Customer cus[], int size){
+     int i = 1;
+     Customer tmp = cus[0];
+     for ( ; i < size; i++){
+         if (cus[i].getWater() > tmp.getWater()){
+            tmp = cus[i];
+         } 
+     }
+     
+     cout<<"\nUser that use a lot of water\n";
+     tmp.output();
+}
+
+void findMostUseElectricNWater(Customer cus[], int size){
+     
+     Customer tmp1 = cus[0];
+     Customer tmp2 = cus[0];
+     //Find water
+     for ( int i = 1; i < size; i++){
+         if (cus[i].getWater() > tmp1.getWater()){
+            tmp1 = cus[i];
+         } 
+     }
+     
+     // Find electric
+     for ( int i = 1; i < size; i++){
+         if (cus[i].getElectric() > tmp2.getElectric()){
+            tmp2 = cus[i];
+         } 
+     }
+     
+     if (tmp1.getId() == tmp2.getId()){
+        cout<<"\n\nUser that use a lot of water and elect:\n";
+        tmp1.output();
+     } else {
+       
+        cout<<"\n\nUser that use a lot of water and elect:\n";
+        cout<<"User not found";
+     }
+     
+}
+
+int main(){
+     Customer cus[3] = {
+              Customer(1, "Daveat", 300.0, 200.0),
+              Customer(2, "God", 500.0, 500.0),
+              Customer(3, "Boy", 200.0, 100.0),
+     };
+     
+     for (int i = 0; i< 3; i++){
+            cus[i].output();
+            cout<<endl;
+     }
+     
+     cout<<"\nResult: \n";
+     
+     findMostUseElectric(cus, 3);
+     
+     findMostUseWater(cus, 3);
+     
+     findMostUseElectricNWater(cus, 3);
+     
+     getch();
 }
